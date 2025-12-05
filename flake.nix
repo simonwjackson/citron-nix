fc87570911c14eefba308ae5b830ef42fc6b63d2
Simon W. Jackson <github@simonwjackson.io>
2025-12-05 10:26:29 -0700

    refactor(flake): move src to flake inputs

--- flake.nix
+++ flake.nix
@@ -3,26 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    citron-src = {
+      url = "git+https://git.citron-emu.org/Citron/Emulator.git?rev=4491abcdcee2a92fe3520db29f5b652006890ecd&submodules=1";
+      flake = false;
+    };
+
+    nx-tzdb = {
+      url = "https://github.com/lat9nq/tzdb_to_nx/releases/download/221202/221202.zip";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          citron = pkgs.callPackage ./package.nix { };
-          default = self.packages.${system}.citron;
-        }
-      );
+  outputs = {
+    self,
+    nixpkgs,
+    citron-src,
+    nx-tzdb,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        citron = pkgs.callPackage ./package.nix {
+          src = citron-src;
+          nx-tzdb = nx-tzdb;
+        };
+        default = self.packages.${system}.citron;
+      }
+    );
 
-      overlays.default = final: prev: {
-        citron = self.packages.${prev.system}.citron;
-      };
+    overlays.default = final: prev: {
+      citron = self.packages.${prev.system}.citron;
     };
+  };
 }
--- package.nix
+++ package.nix
@@ -1,8 +1,8 @@
 {
   lib,
   stdenv,
-  fetchgit,
-  fetchurl,
+  src,
+  nx-tzdb,
   cmake,
   ninja,
   pkg-config,
@@ -32,10 +32,9 @@
   zstd,
   gamemode,
   unzip,
-}:
-
-let
-  inherit (qt6)
+}: let
+  inherit
+    (qt6)
     qtbase
     qtmultimedia
     qttools
@@ -43,137 +42,126 @@ let
     qtwebengine
     wrapQtAppsHook
     ;
-
-  # Pre-downloaded timezone data (can't download in sandbox)
-  nx-tzdb = fetchurl {
-    url = "https://github.com/lat9nq/tzdb_to_nx/releases/download/221202/221202.zip";
-    hash = "sha256-mRzW+iIwrU1zsxHmf+0RArU8BShAoEMvCz+McXFFK3c=";
-  };
 in
-stdenv.mkDerivation (finalAttrs: {
-  pname = "citron";
-  version = "0.11.0";
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "citron";
+    version = "0.11.0";
 
-  src = fetchgit {
-    url = "https://git.citron-emu.org/Citron/Emulator.git";
-    rev = "4491abcdcee2a92fe3520db29f5b652006890ecd"; # v0.11.0
-    hash = "sha256-rnoxh71wsQI4HyqFjzkRdMPe2WpFxmlIa4IodSlBBec=";
-    fetchSubmodules = true;
-  };
+    inherit src;
 
-  nativeBuildInputs = [
-    cmake
-    ninja
-    pkg-config
-    wrapGAppsHook3
-    wrapQtAppsHook
-    glslang
-    qttools
-    unzip
-  ];
+    nativeBuildInputs = [
+      cmake
+      ninja
+      pkg-config
+      wrapGAppsHook3
+      wrapQtAppsHook
+      glslang
+      qttools
+      unzip
+    ];
 
-  buildInputs = [
-    # Qt6
-    qtbase
-    qtmultimedia
-    qtwayland
-    qtwebengine
+    buildInputs = [
+      # Qt6
+      qtbase
+      qtmultimedia
+      qtwayland
+      qtwebengine
 
-    # Core deps
-    SDL2
-    boost
-    enet
-    ffmpeg
-    fmt
-    libopus
-    libusb1
-    libva
-    lz4
-    nlohmann_json
-    openal
-    rapidjson
-    openssl
-    speexdsp
-    vulkan-headers
-    vulkan-loader
-    vulkan-utility-libraries
-    zlib
-    zstd
+      # Core deps
+      SDL2
+      boost
+      enet
+      ffmpeg
+      fmt
+      libopus
+      libusb1
+      libva
+      lz4
+      nlohmann_json
+      openal
+      rapidjson
+      openssl
+      speexdsp
+      vulkan-headers
+      vulkan-loader
+      vulkan-utility-libraries
+      zlib
+      zstd
 
-    # Optional
-    catch2_3
-    cubeb
-    gamemode
-  ];
+      # Optional
+      catch2_3
+      cubeb
+      gamemode
+    ];
 
-  postPatch = ''
-    # Fix for Qt 6.10.0 compatibility
-    substituteInPlace CMakeLists.txt \
-      --replace-fail "find_package(Qt6 REQUIRED COMPONENTS Widgets" \
-                     "find_package(Qt6 REQUIRED COMPONENTS Widgets GuiPrivate"
-    substituteInPlace CMakeLists.txt \
-      --replace-fail "set(CITRON_QT_COMPONENTS2 Core" \
-                     "set(CITRON_QT_COMPONENTS2 Core GuiPrivate"
-    substituteInPlace src/citron/CMakeLists.txt \
-      --replace-fail "target_link_libraries(citron PRIVATE Boost::headers" \
-                     "target_link_libraries(citron PRIVATE Boost::headers Qt6::GuiPrivate"
+    postPatch = ''
+      # Fix for Qt 6.10.0 compatibility
+      substituteInPlace CMakeLists.txt \
+        --replace-fail "find_package(Qt6 REQUIRED COMPONENTS Widgets" \
+                       "find_package(Qt6 REQUIRED COMPONENTS Widgets GuiPrivate"
+      substituteInPlace CMakeLists.txt \
+        --replace-fail "set(CITRON_QT_COMPONENTS2 Core" \
+                       "set(CITRON_QT_COMPONENTS2 Core GuiPrivate"
+      substituteInPlace src/citron/CMakeLists.txt \
+        --replace-fail "target_link_libraries(citron PRIVATE Boost::headers" \
+                       "target_link_libraries(citron PRIVATE Boost::headers Qt6::GuiPrivate"
 
-    # Extract pre-downloaded timezone data to source directory
-    mkdir -p externals/nx_tzdb/
-    unzip -o ${nx-tzdb} -d externals/nx_tzdb/
-  '';
+      # Extract pre-downloaded timezone data to source directory
+      mkdir -p externals/nx_tzdb/
+      unzip -o ${nx-tzdb} -d externals/nx_tzdb/
+    '';
 
-  # Copy timezone data to build directory where cmake expects it
-  preConfigure = ''
-    mkdir -p build/externals/nx_tzdb/nx_tzdb
-    cp -r externals/nx_tzdb/* build/externals/nx_tzdb/nx_tzdb/
-  '';
+    # Copy timezone data to build directory where cmake expects it
+    preConfigure = ''
+      mkdir -p build/externals/nx_tzdb/nx_tzdb
+      cp -r externals/nx_tzdb/* build/externals/nx_tzdb/nx_tzdb/
+    '';
 
-  cmakeFlags = [
-    "-GNinja"
-    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
-    "-DCITRON_USE_BUNDLED_VCPKG=OFF"
-    "-DCITRON_USE_BUNDLED_QT=OFF"
-    "-DCITRON_USE_BUNDLED_FFMPEG=OFF"
-    "-DCITRON_USE_BUNDLED_SDL2=OFF"
-    "-DCITRON_USE_EXTERNAL_SDL2=OFF"
-    "-DCITRON_USE_EXTERNAL_VULKAN_HEADERS=OFF"
-    "-DCITRON_USE_EXTERNAL_VULKAN_UTILITY_LIBRARIES=OFF"
-    "-DCITRON_TESTS=OFF"
-    "-DCITRON_CHECK_SUBMODULES=OFF"
-    "-DCITRON_ENABLE_LTO=OFF"
-    "-DCITRON_USE_QT_MULTIMEDIA=ON"
-    "-DCITRON_USE_QT_WEB_ENGINE=ON"
-    "-DCITRON_DOWNLOAD_TIME_ZONE_DATA=OFF"
-    "-DENABLE_QT_TRANSLATION=ON"
-    "-DUSE_DISCORD_PRESENCE=ON"
-    "-DCITRON_USE_FASTER_LD=OFF"
-    "-DTITLE_BAR_FORMAT_RUNNING=citron | ${finalAttrs.version} {}"
-    "-DTITLE_BAR_FORMAT_IDLE=citron | ${finalAttrs.version}"
-  ];
+    cmakeFlags = [
+      "-GNinja"
+      "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+      "-DCITRON_USE_BUNDLED_VCPKG=OFF"
+      "-DCITRON_USE_BUNDLED_QT=OFF"
+      "-DCITRON_USE_BUNDLED_FFMPEG=OFF"
+      "-DCITRON_USE_BUNDLED_SDL2=OFF"
+      "-DCITRON_USE_EXTERNAL_SDL2=OFF"
+      "-DCITRON_USE_EXTERNAL_VULKAN_HEADERS=OFF"
+      "-DCITRON_USE_EXTERNAL_VULKAN_UTILITY_LIBRARIES=OFF"
+      "-DCITRON_TESTS=OFF"
+      "-DCITRON_CHECK_SUBMODULES=OFF"
+      "-DCITRON_ENABLE_LTO=OFF"
+      "-DCITRON_USE_QT_MULTIMEDIA=ON"
+      "-DCITRON_USE_QT_WEB_ENGINE=ON"
+      "-DCITRON_DOWNLOAD_TIME_ZONE_DATA=OFF"
+      "-DENABLE_QT_TRANSLATION=ON"
+      "-DUSE_DISCORD_PRESENCE=ON"
+      "-DCITRON_USE_FASTER_LD=OFF"
+      "-DTITLE_BAR_FORMAT_RUNNING=citron | ${finalAttrs.version} {}"
+      "-DTITLE_BAR_FORMAT_IDLE=citron | ${finalAttrs.version}"
+    ];
 
-  # Ensure Vulkan loader is found at runtime
-  qtWrapperArgs = [
-    "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ vulkan-loader ]}"
-  ];
+    # Ensure Vulkan loader is found at runtime
+    qtWrapperArgs = [
+      "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [vulkan-loader]}"
+    ];
 
-  dontWrapGApps = true;
+    dontWrapGApps = true;
 
-  preFixup = ''
-    qtWrapperArgs+=("''${gappsWrapperArgs[@]}")
-  '';
+    preFixup = ''
+      qtWrapperArgs+=("''${gappsWrapperArgs[@]}")
+    '';
 
-  postInstall = ''
-    # Install udev rules for input devices
-    install -Dm644 $src/dist/72-citron-input.rules $out/lib/udev/rules.d/72-citron-input.rules
-  '';
+    postInstall = ''
+      # Install udev rules for input devices
+      install -Dm644 $src/dist/72-citron-input.rules $out/lib/udev/rules.d/72-citron-input.rules
+    '';
 
-  meta = {
-    description = "Nintendo Switch emulator forked from yuzu";
-    homepage = "https://citron-emu.org";
-    license = lib.licenses.gpl2Plus;
-    maintainers = [ ];
-    platforms = [ "x86_64-linux" ];
-    mainProgram = "citron";
-  };
-})
+    meta = {
+      description = "Nintendo Switch emulator forked from yuzu";
+      homepage = "https://citron-emu.org";
+      license = lib.licenses.gpl2Plus;
+      maintainers = [];
+      platforms = ["x86_64-linux"];
+      mainProgram = "citron";
+    };
+  })
